c1527e2a237321c1255d2718966a7dbaac274922
Kyle K <doctor.whom@gmail.com>
2019-06-25 20:34:03 +0000

    Finding a better location for marker data

--- src/main/target/unified_config_marker.c
+++ src/main/target/unified_config_marker.c
@@ -3,5 +3,5 @@
 #ifndef UNIFIED_REGION
 #    define UNIFIED_REGION 0x08003000
 #endif
-#define UNIFIED_MARKER "#BTFLUNIFIEDCFG\n#ST " STR(UNIFIED_REGION) "\n#LN " STR(4096) "\n"
-const char unifiedConfigMarker[] = UNIFIED_MARKER;
+#define UNIFIED_MARKER "#BTFLUNIFIEDCFG\n#STA " STR(UNIFIED_REGION) "\n#LN " STR(4096) "\n"
+char unifiedConfigMarker[] = UNIFIED_MARKER;
